48b5c8e47976fa180fb32c16add421ea63c6d433
volkoshkursk <zaz-pagani@inbox.ru>
2023-06-30 13:41:36 +0300

    remove old code from C-extended part

--- mi/mi1.c
+++ mi/mi1.c
@@ -43,22 +43,24 @@ long double mi(char** news, unsigned n2, char* class_, char** classes, char* wor
 			}
 		}
 	}
-	if((N11*N10*N00*N01) != 0)
-	{
-		long double N = N11+N10+N00+N01;
-		long double N1x = N11+N10;
-		long double Nx1 = N11 + N01;
-		long double N0x = N01+N00;
-		long double Nx0 = N10 + N00;
-		return (((N11/N)*logarithm((N*N11)/(N1x*Nx1))) + ((N01/N) * logarithm((N*N01)/(N0x*Nx1))) + ((N10/N) * logarithm((N*N10)/(N1x*Nx0))) + ((N00/N) * logarithm((N*N00)/(N0x*Nx0))));
+	long double N = N11+N10+N00+N01;
+	long double N1x = N11+N10;
+	long double Nx1 = N11 + N01;
+	long double N0x = N01+N00;
+	long double Nx0 = N10 + N00;
+
+	long double result = 0;
+	if (N1x*Nx1 > 0) {
+		result += (N11/N)*logarithm((N*N11)/(N1x*Nx1));
 	}
-	else
-	{
-	    if (N11 == 0)
-		    return -1;
-		else
-		{
-		    return -2;
-		}
+	if (N0x*Nx1 > 0) {
+		result += (N01/N) * logarithm((N*N01)/(N0x*Nx1));
 	}
+	if (N1x*Nx0 > 0) {
+		result += (N10/N) * logarithm((N*N10)/(N1x*Nx0));
+	}
+	if (N0x*Nx0 > 0) {
+		result += (N00/N) * logarithm((N*N00)/(N0x*Nx0));
+	}
+	return result;
 }
